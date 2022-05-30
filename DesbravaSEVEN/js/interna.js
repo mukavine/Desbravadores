90b59dfd1ee820611eef7f6edaab912027753f66
Gabriel Maximiano <gabriel.mgomes@sptech.school>
2022-05-30 12:47:21 -0300

    modificaçoes de pasta

--- DesbravaSEVEN/js/interna.js
+++ DesbravaSEVEN/js/interna.js
@@ -1,5 +1,4 @@
 // validaçoes de troca de setor fisico, mental, e espiritual
-<<<<<<< HEAD
 function trilha() {
     seleçao_trilha.style.display = ""
     seleçao_grafico.style.display = "none"
@@ -69,35 +68,3 @@ function calcular_tempo() {
 
 }
 
-=======
-function trilha(){
-    seleçao_trilha.style.display=""
-    seleçao_grafico.style.display="none"
-    seleçao_mestrados.style.display="none"
-    
-    
-}
-function grafico(){
-
-    seleçao_trilha.style.display="none"
-    seleçao_grafico.style.display=""
-    seleçao_mestrados.style.display="none"
-    
-    var select = select_grafico.value;
-    
-    if(select == 'barra'){
-        seleçao_barra.style.display="";
-        seleçao_porcentagem.style.display="none";
-    }else{
-        seleçao_barra.style.display="none"
-        seleçao_porcentagem.style.display=""
-    }
-    
-}
-function mestrado(){
-    seleçao_trilha.style.display="none"
-    seleçao_grafico.style.display="none"
-    seleçao_mestrados.style.display=""
-    
-}
->>>>>>> 374962950cec2ca1f24347344e3b57223dfe5c3c
