d2e5468181ca87bc7569f93bddd3b4686e32a533
mukavine <99892006+mukavine@users.noreply.github.com>
2022-05-30 01:51:49 -0300

    site inteiro codado e validaçoes feitas

--- DesbravaSEVEN/js/interna.js
+++ DesbravaSEVEN/js/interna.js
@@ -1,4 +1,75 @@
 // validaçoes de troca de setor fisico, mental, e espiritual
+<<<<<<< HEAD
+function trilha() {
+    seleçao_trilha.style.display = ""
+    seleçao_grafico.style.display = "none"
+    seleçao_mestrados.style.display = "none"
+
+
+}
+function grafico() {
+
+    seleçao_trilha.style.display = "none"
+    seleçao_grafico.style.display = ""
+    seleçao_mestrados.style.display = "none"
+
+    var select = select_grafico.value;
+
+    if (select == 'barra') {
+        seleçao_barra.style.display = "";
+        seleçao_porcentagem.style.display = "none";
+    } else {
+        seleçao_barra.style.display = "none"
+        seleçao_porcentagem.style.display = ""
+    }
+
+}
+function mestrado() {
+    seleçao_trilha.style.display = "none"
+    seleçao_grafico.style.display = "none"
+    seleçao_mestrados.style.display = ""
+
+}
+
+function calcular_tempo() {
+    var acumulador = 0;
+    var mestrado = 0;
+    var mestrados = Number(input_mestrado.value);
+    var especialidade = Number(input_especialidades_por_mes.value);
+    var qtd_esp = 0;
+    var parar_contador = false;
+    div_for.innerHTML = ``
+
+
+  
+        for (var cont = 1; cont <= 50; cont++) {
+
+            acumulador += especialidade
+
+            if (acumulador >= 7) {
+                acumulador = 0
+                mestrado++
+
+            }
+            qtd_esp += especialidade
+
+            div_for.innerHTML += `
+                               ${cont}° Mês <br>
+                               ${qtd_esp} Especialidades concluidas<br>
+                               ${mestrado}mestrado<br>`
+
+            if (mestrado == mestrados) {
+                cont = 51;
+            }
+
+
+        
+    }
+
+
+}
+
+=======
 function trilha(){
     seleçao_trilha.style.display=""
     seleçao_grafico.style.display="none"
@@ -29,3 +100,4 @@ function mestrado(){
     seleçao_mestrados.style.display=""
     
 }
+>>>>>>> 374962950cec2ca1f24347344e3b57223dfe5c3c
